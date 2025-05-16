8c36e322404b6d93fcd1aea3e27d13fd60c3a2e2
Lakshaydang47 <139232283+Lakshaydang47@users.noreply.github.com>
2025-05-16 14:15:30 +0530

    Update PracticeQuestions.kt
    question 4 to 6

--- PracticeQuestions.kt
+++ PracticeQuestions.kt
@@ -5,6 +5,12 @@ fun main() {
     Ques_2()
     println("\nSolution of 3rd ques")
     Ques_3()
+    println("\nSolution of 4th ques")
+    Ques_4()
+    println("\nSolution of 5th ques")
+    Ques_5()
+    println("\nSolution of 6th ques")
+    Ques_6()
 }
 
 // 1...Print messages
@@ -58,3 +64,90 @@ fun Ques_3(){
     println(offer)
 }
 
+// 4...String concatenation
+// This program displays the total party size. There are adults and kids at the party. The numberOfAdults variable holds the number of adults at the party and the numberOfKids variable holds the number of kids.
+
+
+// fun main() {
+//     val numberOfAdults = "20"
+//     val numberOfKids = "30"
+//     val total = numberOfAdults + numberOfKids
+//     println("The total party size is: $total")
+// }
+
+fun Ques_4() {
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}
+
+// 5... Message formatting
+// fun main() {
+//     val baseSalary = 5000
+//     val bonusAmount = 1000
+//     val totalSalary = "$baseSalary + $bonusAmount"
+//     println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+// }
+// Can you figure out the output of this code before you run it in Kotlin Playground?
+// When you run the code in Kotlin Playground, does it print the output that you expected?
+
+fun Ques_5() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
+
+//  6...Implement basic math operations
+// In this exercise, you write a program that performs basic math operations and prints the output.
+
+// Step 1
+// This main() function contains one compile error:
+
+
+// fun main() {
+//     val firstNumber = 10
+//     val secondNumber = 5
+    
+//     println("$firstNumber + $secondNumber = $result")
+// }
+// Can you fix the error so that the program prints this output?
+
+// 10 + 5 = 15
+// Step 2
+// The code works, but the logic for adding two numbers is located within the result 
+// variable, making your code less flexible to reuse. Instead, you can extract the
+//  addition operation into an add() function so that the code is reusable. 
+//  To do this, update your code with the code listed below. Notice that the code 
+//  now introduces a new val called thirdNumber and prints the result of this new variable with firstNumber.
+
+fun Ques_6() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+    
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = add(firstNumber, thirdNumber)
+
+    val result2 = sub(firstNumber, secondNumber)
+    val anotherResult2 = sub(firstNumber, thirdNumber)
+    
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
+
+    println("$firstNumber - $secondNumber = $result2")
+    println("$firstNumber - $thirdNumber = $anotherResult2")
+}
+
+// Define add() function below this line
+fun add(a: Int, b: Int): Int {
+    val sum = a+b
+    return sum
+}
+
+// Define sub() function below this line
+fun sub(a: Int, b: Int): Int {
+    val diff = a-b
+    return diff
+}
